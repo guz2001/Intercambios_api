7d60720c493e834caaa7e6d2629ee5aef7fcc27d
guz2001 <guz20dgc@gmail.com>
2026-03-23 21:24:47 -0500

    clase requerimientos atributos

--- src/main/java/com/intercambios/intercambios_api/model/RequerimientosEnergeticos.java
+++ src/main/java/com/intercambios/intercambios_api/model/RequerimientosEnergeticos.java
@@ -1,4 +1,21 @@
 package com.intercambios.intercambios_api.model;
 
 public class RequerimientosEnergeticos {
+    //private double caloriasDiarias;
+    private double peso;
+    private double altura;
+    private int edad;
+    private String sexo;
+    private double factorActividad;
+    public RequerimientosEnergeticos(double peso,double altura,int  edad,String sexo,double factorActividad){
+        this.caloriasDiarias=caloriasDiarias;
+    }
+
+    public double getCaloriasDiarias() {
+        return caloriasDiarias;
+    }
+
+    public void setCaloriasDiarias(double caloriasDiarias) {
+        this.caloriasDiarias = caloriasDiarias;
+    }
 }
