d9bc19b647fe427b23a9440a51bdab1acd328ee6
guz2001 <guz20dgc@gmail.com>
2026-03-23 21:26:52 -0500

    clase requerimientos getters y setters

--- src/main/java/com/intercambios/intercambios_api/model/RequerimientosEnergeticos.java
+++ src/main/java/com/intercambios/intercambios_api/model/RequerimientosEnergeticos.java
@@ -8,14 +8,51 @@ public class RequerimientosEnergeticos {
     private String sexo;
     private double factorActividad;
     public RequerimientosEnergeticos(double peso,double altura,int  edad,String sexo,double factorActividad){
-        this.caloriasDiarias=caloriasDiarias;
+        //this.caloriasDiarias=caloriasDiarias;
+        this.peso=peso;
+        this.altura=altura;
+        this.edad=edad;
+        this.sexo=sexo;
+        this.factorActividad=factorActividad;
     }
 
-    public double getCaloriasDiarias() {
-        return caloriasDiarias;
+    public double getPeso() {
+        return peso;
     }
 
-    public void setCaloriasDiarias(double caloriasDiarias) {
-        this.caloriasDiarias = caloriasDiarias;
+    public double getAltura() {
+        return altura;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public double getFactorActividad() {
+        return factorActividad;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setFactorActividad(double factorActividad) {
+        this.factorActividad = factorActividad;
     }
 }
