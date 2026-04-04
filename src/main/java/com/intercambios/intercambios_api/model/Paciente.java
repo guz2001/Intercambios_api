6f7a04f57aca787480e86443042ecdc2f4554238
guz2001 <guz20dgc@gmail.com>
2026-04-03 22:29:34 -0500

    establecemos los valores de la tabla paciente

--- src/main/java/com/intercambios/intercambios_api/model/Paciente.java
+++ src/main/java/com/intercambios/intercambios_api/model/Paciente.java
@@ -1,4 +1,19 @@
 package com.intercambios.intercambios_api.model;
+import jakarta.persistence.*;
+@Entity
+@Table(name = "paciente")
 
 public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    //validamos la columna y establecemos que no puede estar vacia
+    @Column(nullable = false, length=200)
+    private String nombre;
+
+    @Column(nullable = false)
+    private  Integer edad;
+
+
+
 }
