212774389e286abe6a02cbe93fbe733fb86cdea0
guz2001 <guz20dgc@gmail.com>
2026-04-03 22:38:08 -0500

    Registramos las filas de la tabla paciente y documentamos el por que se creo la clase sexo

--- src/main/java/com/intercambios/intercambios_api/model/Paciente.java
+++ src/main/java/com/intercambios/intercambios_api/model/Paciente.java
@@ -14,6 +14,20 @@ public class Paciente {
     @Column(nullable = false)
     private  Integer edad;
 
+    @Column(nullable = false)
+    private  Double peso;
+
+    @Column(nullable = false)
+    private  Double altura;
+
+    @Column(name= "factor_actividad", nullable = false)
+    private Double factorActividad;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private  Sexo sexo;// como sexo puede variar llamamos la clase sexo donde estaran
+    //almacenados las formulas de harrys-benedict para hombre y mujer, y para menores de 2 años
+    // otras formulas
+
 
 
 }
